ade7ddadf47c598961d064e367c18f6fd706263d
danillkifly <danillkifly@gmail.com>
2025-05-10 12:19:24 +0700

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const input = document.getElementById("passwordInput");
+const input = document.getElementById("PINInput");
+const loadingOverlay = document.getElementById("loading-overlay");
 
-// Daftar password yang diterima
 const allowedPasswords = ["111111", "admin1", "kifly1"];
 
 input.addEventListener("input", function () {
   if (input.value.length === 6) {
     if (allowedPasswords.includes(input.value)) {
-      window.location.href = "https://danillkifly.github.io/"; // Ganti ke halaman tujuanmu
+      loadingOverlay.style.display = "flex"; // Tampilkan loading
+      setTimeout(() => {
+        window.location.href = "https://danillkifly.github.io/";
+      }, 2000); // Tunda 2 detik sebelum pindah halaman
     } else {
       alert("Password salah!");
-      input.value = ""; // Kosongkan input setelah salah
+      input.value = "";
     }
   }
 });
